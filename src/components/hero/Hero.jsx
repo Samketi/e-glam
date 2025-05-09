e7e329d6657a000b9f0ba86ee45d6af85d004751
Samketi <nkosisamketi5@gmail.com>
2025-05-09 12:32:12 +0200

    push

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,18 @@
 import React, { useState, useEffect } from "react";
-import hero1 from "../../assets/img/hero2.jpg";
-import hero2 from "../../assets/img/h6.jpg";
-import hero3 from "../../assets/img/h7.jpg";
+
+import hero1 from "../../assets/img/hero.jpg";
+import hero2 from "../../assets/img/hero.jpg";
 
 const slides = [
   {
     image: hero1,
-    heading: "Transform Your Look, Unleash Your Beauty",
-    text: "Welcome to Excellent Glam Artistry, where elegance meets creativity. I am dedicated to providing personalized makeup services that make you feel confident and radiant. Let your beauty shine with a touch of artistry.",
+    heading: "Alex Steffen",
+    text: "Recycling is a good thing to do. It makes us feel better, but it doesn't really address the core issue of our throwaway culture.",
   },
   {
     image: hero2,
-    heading: "Discover Your Unique Style",
-    text: "At Excellent Glam, we celebrate individuality. Our expert makeup services highlight your unique features and bring out your natural beauty. Experience a transformation that is uniquely you.",
-  },
-  {
-    image: hero3,
-    heading: "Radiate Confidence with Every Look",
-    text: "Confidence starts with how you feel. Our makeup artistry aims to empower you by enhancing your natural beauty. Step out with confidence and make every moment memorable with Excellent Glam.",
+    heading: "Anonymous",
+    text: "Recycling turns things into other things, which is like magic.",
   },
 ];
 
@@ -33,7 +28,7 @@ const Hero = () => {
 
   return (
     <div className="relative h-screen flex items-center justify-center">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-70"></div>
       <div className="relative z-10 text-center p-4">
         <h1 className="mt-[5%] text-7xl font-extrabold text-white animate-slideTop">
           {slides[currentSlide].heading}
